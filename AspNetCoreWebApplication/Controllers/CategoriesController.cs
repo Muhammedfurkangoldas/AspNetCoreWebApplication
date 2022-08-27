4aff4c6934d693e8f399f3a8f6600ee81062f669
muhammed furkan göldaş <muhammedfg98@gmail.com>
2022-08-27 10:02:29 +0300

    Add project files.

---
+++ AspNetCoreWebApplication/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreWebApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreWebApplication.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public CategoriesController(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public IActionResult Index(int? id)
+        {
+            if (id == null) return NotFound(); // adres çubuğundan id gönderilmemişse ekrana sayfa bulunamadı hatası dön
+
+            if (id != null) // Eğer adres çubuğunda Products controller a id gönderilmişse
+            {
+                return View(_databaseContext.Categories.Include(c => c.Products).FirstOrDefault(p => p.Id == id)); // Kategorilerden ıd si adres çubuğundan gelen id ile eşleşen kaydı getir ve o kategorinin ürünlerini de kayda include ile dahil et
+            }
+            return View();
+        }
+    }
+}
---
+++ AspNetCoreWebApplication/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using AspNetCoreWebApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreWebApplication.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public ProductsController(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public IActionResult Index(int? id)
+        {
+
+            return View(_databaseContext.Products.ToList());
+        }
+        public async Task<IActionResult> DetailAsync(int? id)
+        {
+            if (id == null) return BadRequest();// Eğer Adres çubuğunda id değeri gönderilmemişse geriye geçersiz istek hatası dön.
+           
+            return View(await _databaseContext.Products.Include("Brand").Include(c=>c.Category).FirstOrDefaultAsync(p=>p.Id == id));
+        }
+    }
+}
